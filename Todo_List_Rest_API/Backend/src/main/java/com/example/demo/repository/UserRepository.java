bfbd0ed12b332fa794294889e9db2a7dcc678471
Ashish Kumar <kumarashish79924@gmail.com>
2021-05-10 00:29:42 +0530

    Delete Todo_List_Rest_API/Backend directory

--- Todo_List_Rest_API/Backend/src/main/java/com/example/demo/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Users, Integer> {
-
-
-}
